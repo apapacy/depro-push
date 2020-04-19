10dfe41b0a3ab3f2b8cfe46cc06761299c0cb710
andrey.ovcharenko@branderstudio.com <andrey.ovcharenko@branderstudio.com>
2020-04-19 18:16:06 +0300

    all for push user adn topics

--- src/main/java/com/deprosystem/push/controller/IndexController.java
+++ src/main/java/com/deprosystem/push/controller/IndexController.java
@@ -7,7 +7,7 @@ package com.deprosystem.push.controller;
 
 
 import com.deprosystem.push.bean.FireBaseConfig;
-import com.deprosystem.push.firebse.FirebaseManager;
+import com.deprosystem.push.firebase.FirebaseManager;
 import com.deprosystem.push.model.FirebaseTokenRepository;
 import com.deprosystem.push.model.FirebaseToken;
 import com.deprosystem.push.model.FirebaseTopic;
@@ -131,7 +131,7 @@ public class IndexController {
         return new Output();
     }
 
-    @PostMapping("/send-to-usser")
+    @PostMapping("/send-to-user")
     public Output sendToTopic(@RequestBody InputMessage input) {
         List<FirebaseToken> tokens = this.firebaseTokenRepository.findByUserId(input.userId);
         for (int i = 0; i < tokens.size(); i++)
@@ -143,8 +143,7 @@ public class IndexController {
              return new Output();
     }
 
-
-        @PostMapping("/send-to-topic")
+    @PostMapping("/send-to-topic")
     public Output sendToUser(@RequestBody InputMessage input) {
         try {
             FirebaseManager.sendMessageToTopic(input.topic, input.title, input.body, input.data);
@@ -154,94 +153,4 @@ public class IndexController {
             return new Output(ex.getMessage());
         }
     }
-
-
-    
-    @GetMapping("/test")
-    public String index() throws FirebaseMessagingException {
-        Map<String, String> model = new HashMap<>();
-        model.put("name", "Alexey++--");
-        // This registration token comes from the client FCM SDKs.
-String registrationToken = "fAKGIDRybgLMy-bc3WdV2C:APA91bF-ybDZU-ws3br72nVFO-oUDyLefVwfMkCXmwenUJjBR5KkQY4zUzI_5eOiEcqQgVJssf6vfkGePzGtIbEQmAktZRubeVeOQJm8NiFDSxyRd5IhS994wM1fYGedTIyT7JqFHAk3";
-
-// See documentation on defining a message payload.
-Message message = Message.builder()
-      .setNotification(new Notification("title", "body"))
-      .setAndroidConfig(AndroidConfig.builder()
-          .setNotification(AndroidNotification.builder()
-              //.setIcon(ANDROID_NEWS_ICON_RESOURCE)
-              .build())
-          .build())
-      .setApnsConfig(ApnsConfig.builder()
-          .setAps(Aps.builder()
-              //.setBadge(APNS_NEWS_BADGE_RESOURCE)
-              .build())
-          .build())
-      .setWebpushConfig(WebpushConfig.builder()
-          //.setNotification(new WebpushNotification(null, null, WEBPUSH_NEWS_ICON_URL))
-          .build())
-      // .setTopic("auto-news")
-      .setToken(registrationToken)
-    .build();
-
-// Send a message to the device corresponding to the provided
-// registration token.
-String response = FirebaseMessaging.getInstance().send(message);
-// Response is a message ID string.
-System.out.println("Successfully sent message: " + response);
-        
-        
-        
-
-        //Visit visit = new Visit();
-        //visit.description = String.format("Visited at %s", LocalDateTime.now());
-        //visitsRepository.save(visit);
-
-        return "index++";
-    }
-
-    @GetMapping("/test1")
-    public String test1() throws FirebaseMessagingException {
-        Map<String, String> model = new HashMap<>();
-        model.put("name", "Alexey++--");
-        // This registration token comes from the client FCM SDKs.
-String registrationToken = "fAKGIDRybgLMy-bc3WdV2C:APA91bF-ybDZU-ws3br72nVFO-oUDyLefVwfMkCXmwenUJjBR5KkQY4zUzI_5eOiEcqQgVJssf6vfkGePzGtIbEQmAktZRubeVeOQJm8NiFDSxyRd5IhS994wM1fYGedTIyT7JqFHAk3";
-
-// See documentation on defining a message payload.
-Message message = Message.builder()
-      .setNotification(new Notification("title", "body"))
-      .setAndroidConfig(AndroidConfig.builder()
-          .setNotification(AndroidNotification.builder()
-              //.setIcon(ANDROID_NEWS_ICON_RESOURCE)
-              .build())
-          .build())
-      .setApnsConfig(ApnsConfig.builder()
-          .setAps(Aps.builder()
-              //.setBadge(APNS_NEWS_BADGE_RESOURCE)
-              .build())
-          .build())
-      .setWebpushConfig(WebpushConfig.builder()
-          //.setNotification(new WebpushNotification(null, null, WEBPUSH_NEWS_ICON_URL))
-          .build())
-      // .setTopic("auto-news")
-      .setTopic("a")
-    .build();
-
-// Send a message to the device corresponding to the provided
-// registration token.
-String response = FirebaseMessaging.getInstance().send(message);
-// Response is a message ID string.
-System.out.println("Successfully sent message: " + response);
-        
-        
-        
-
-        //Visit visit = new Visit();
-        //visit.description = String.format("Visited at %s", LocalDateTime.now());
-        //visitsRepository.save(visit);
-
-        return "index++";
-    }
-
-
 }
